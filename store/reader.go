a14b100fff40e381f6bdf54d6093fa2633d4a99e
yyyogev <yogev.metzuyanim@logz.io>
2019-11-26 12:15:40 +0200

    implement multiread

--- store/reader.go
+++ store/reader.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/jaegertracing/jaeger/pkg/es"
 	"github.com/jaegertracing/jaeger/plugin/storage/es/spanstore/dbmodel"
 	"github.com/olivere/elastic"
@@ -11,6 +12,9 @@ import (
 	ottag "github.com/opentracing/opentracing-go/ext"
 	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-hclog"
@@ -27,15 +31,34 @@ type LogzioSpanReader struct {
 	spanConverter	dbmodel.ToDomain
 }
 
+type logzioSearchRequest struct {
+	query	map[string]interface{}
+	size	int
+	sort	[]map[string]interface{}
+}
+
 // NewLogzioSpanReader creates a new logzio span reader
 func NewLogzioSpanReader(config LogzioConfig, logger hclog.Logger) *LogzioSpanReader {
 	return &LogzioSpanReader{
 		logger:   logger,
 		apiToken: config.APIToken,
+		sourceFn: getSourceFn(),
 	}
 }
 
 type sourceFn func(query elastic.Query, nextTime uint64) *elastic.SearchSource
+
+func getSourceFn() sourceFn {
+	return func(query elastic.Query, nextTime uint64) *elastic.SearchSource {
+		searchSource := elastic.NewSearchSource().
+			Query(query).
+			Size(10000)
+			//TerminateAfter(10000)
+			searchSource.Sort("startTime", true)
+			//SearchAfter(nextTime)
+		return searchSource
+	}
+}
 
 // GetTrace returns a Jaeger trace by traceID
 func (reader *LogzioSpanReader) GetTrace(ctx context.Context, traceID model.TraceID) (*model.Trace, error) {
@@ -104,50 +127,84 @@ func (*LogzioSpanReader) FindTraceIDs(ctx context.Context, query *spanstore.Trac
 }
 
 func (reader *LogzioSpanReader) multiRead(ctx context.Context, traceIDs []model.TraceID, startTime, endTime time.Time) ([]*model.Trace, error) {
+	reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1")
 
 	childSpan, _ := opentracing.StartSpanFromContext(ctx, "multiRead")
 	childSpan.LogFields(otlog.Object("trace_ids", traceIDs))
 	defer childSpan.Finish()
-
+	reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2")
 	if len(traceIDs) == 0 {
 		return []*model.Trace{}, nil
 	}
 
 	nextTime := model.TimeAsEpochMicroseconds(startTime)
+	reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA3")
 
 	searchAfterTime := make(map[model.TraceID]uint64)
 	totalDocumentsFetched := make(map[model.TraceID]int)
 	tracesMap := make(map[model.TraceID]*model.Trace)
+	reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4")
+
 	for {
+		reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4.1 " + string(len(traceIDs)))
+
 		if len(traceIDs) == 0 {
 			break
 		}
+		reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4 " + string(len(traceIDs)))
+
 		searchRequests := make([]*elastic.SearchRequest, len(traceIDs))
 		for i, traceID := range traceIDs {
-			query := elastic.NewTermQuery("traceID", traceID.String())
+			reader.logger.Error(traceID.String())
+
+			traceIdTerm := elastic.NewTermQuery("traceID", traceID.String())
+			rangeQuery := elastic.NewRangeQuery("startTime").Gte(model.TimeAsEpochMicroseconds(startTime)).Lte(model.TimeAsEpochMicroseconds(endTime))
+			query := elastic.NewBoolQuery().Filter(traceIdTerm, rangeQuery)
 			if val, ok := searchAfterTime[traceID]; ok {
 				nextTime = val
 			}
+			reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4.6 " + string(len(traceIDs)))
 
-			reader := reader.sourceFn(query, nextTime)
-
+			source := reader.sourceFn(query, nextTime)
 			searchRequests[i] = elastic.NewSearchRequest().
-				IgnoreUnavailable(true).
-				Source(reader)
+				IgnoreUnavailable(true).Source(source)
+
+			reader.logger.Error(searchRequests[i].Body())
 		}
+		reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA5")
+
 		// set traceIDs to empty
 		traceIDs = nil
-		results, err := reader.client.MultiSearch().Add(searchRequests...).Do(ctx)
-		if err != nil {
-			logErrorToSpan(childSpan, err)
-			return nil, err
-		}
+		reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA5.5")
 
-		if results.Responses == nil || len(results.Responses) == 0 {
-			break
-		}
+		client := &http.Client{}
+		reqReader, _ := searchRequests[0].Body()
+		strBody := fmt.Sprintf("{}\n%s\n", reqReader)
 
+		req, _ := http.NewRequest("POST","https://api-eu.logz.io/v1/elasticsearch/_msearch",strings.NewReader(strBody))
+		req.Header.Add("X-API-TOKEN", reader.apiToken)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		reader.logger.Error(strBody)
+		reader.logger.Error(string(body))
+
+		//results, err := logzioListenerClient.MultiSearch().Headers(apiTokenHeader).Add(searchRequests...).Do(ctx)
+		////results, err := reader.client.MultiSearch().Add(searchRequests...).Do(ctx)
+		//if err != nil {
+		//	logErrorToSpan(childSpan, err)
+		//	return nil, err
+		//}
+		reader.logger.Error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA6")
+
+		//if results.Responses == nil || len(results.Responses) == 0 {
+		//	break
+		//}
+		var results elastic.MultiSearchResult
+		json.Unmarshal(body, &results)
 		for _, result := range results.Responses {
+
+
 			if result.Hits == nil || len(result.Hits.Hits) == 0 {
 				continue
 			}
@@ -183,86 +240,6 @@ func (reader *LogzioSpanReader) multiRead(ctx context.Context, traceIDs []model.
 func (*LogzioSpanReader) GetDependencies(endTs time.Time, lookback time.Duration) ([]model.DependencyLink, error) {
 	return nil, nil
 }
-
-//func (sr *LogzioSpanReader) multiRead(ctx context.Context, traceIDs []model.TraceID, startTime, endTime time.Time) ([]*model.Trace, error) {
-//
-//	childSpan, _ := opentracing.StartSpanFromContext(ctx, "multiRead")
-//	childSpan.LogFields(otlog.Object("trace_ids", traceIDs))
-//	defer childSpan.Finish()
-//
-//	if len(traceIDs) == 0 {
-//		return []*model.Trace{}, nil
-//	}
-//
-//	// Add an hour in both directions so that traces that straddle two indexes are retrieved.
-//	// i.e starts in one and ends in another.
-//	nextTime := model.TimeAsEpochMicroseconds(startTime.Add(-time.Hour))
-//
-//	searchAfterTime := make(map[model.TraceID]uint64)
-//	totalDocumentsFetched := make(map[model.TraceID]int)
-//	tracesMap := make(map[model.TraceID]*model.Trace)
-//	for {
-//		if len(traceIDs) == 0 {
-//			break
-//		}
-//		searchRequests := make([]*elastic.SearchRequest, len(traceIDs))
-//		for i, traceID := range traceIDs {
-//			query := elastic.NewTermQuery("traceID", traceID.String())
-//			if val, ok := searchAfterTime[traceID]; ok {
-//				nextTime = val
-//			}
-//
-//			s := sr.sourceFn(query, nextTime)
-//
-//			searchRequests[i] = elastic.NewSearchRequest().
-//				IgnoreUnavailable(true).
-//				Type(spanType).
-//				Source(s)
-//		}
-//		// set traceIDs to empty
-//		traceIDs = nil
-//		results, err := sr.client.MultiSearch().Add(searchRequestreader...).Index(indicereader...).Do(sr.ctx)
-//
-//		if err != nil {
-//			logErrorToSpan(childSpan, err)
-//			return nil, err
-//		}
-//
-//		if results.Responses == nil || len(results.Responses) == 0 {
-//			break
-//		}
-//
-//		for _, result := range results.Responses {
-//			if result.Hits == nil || len(result.Hits.Hits) == 0 {
-//				continue
-//			}
-//			spans, err := sr.collectSpans(result.Hits.Hits)
-//			if err != nil {
-//				logErrorToSpan(childSpan, err)
-//				return nil, err
-//			}
-//			lastSpan := spans[len(spans)-1]
-//
-//			if traceSpan, ok := tracesMap[lastSpan.TraceID]; ok {
-//				traceSpan.Spans = append(traceSpan.Spans, spanreader...)
-//			} else {
-//				tracesMap[lastSpan.TraceID] = &model.Trace{Spans: spans}
-//			}
-//
-//			totalDocumentsFetched[lastSpan.TraceID] = totalDocumentsFetched[lastSpan.TraceID] + len(result.Hits.Hits)
-//			if totalDocumentsFetched[lastSpan.TraceID] < int(result.TotalHits()) {
-//				traceIDs = append(traceIDs, lastSpan.TraceID)
-//				searchAfterTime[lastSpan.TraceID] = model.TimeAsEpochMicroseconds(lastSpan.StartTime)
-//			}
-//		}
-//	}
-//
-//	var traces []*model.Trace
-//	for _, trace := range tracesMap {
-//		traces = append(traces, trace)
-//	}
-//	return traces, nil
-//}
 
 func logErrorToSpan(span opentracing.Span, err error) {
 	ottag.Error.Set(span, true)
