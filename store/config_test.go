7245811ab8f3910ebab54b41e417b8b5a53e7106
yyyogev <yogev.metzuyanim@logz.io>
2019-12-02 15:45:42 +0200

    region and config tests
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- store/config_test.go
+++ store/config_test.go
@@ -5,28 +5,65 @@ import (
 )
 
 const (
-	testToken = "testToken"
-	testURL = "testURL"
+	testAccountToken = "testAccountToken"
+	testAPIToken     = "testApiToken"
+	testRegion       = "testRegion"
 )
 
 func TestValidate(tester *testing.T) {
 	config := LogzioConfig{
-		AccountToken:	"",
-		ListenerURL:	"",
+		AccountToken: "",
+		APIToken:     "",
+		Region:       "",
 	}
 	err := config.Validate()
 	if err == nil {
-		tester.Error("validation failed, empty account token should produce error")
+		tester.Error("validation failed, empty account token and api token should produce error")
 	}
 
-	if config.ListenerURL != "https://listener.logz.io:8071" {
-		tester.Errorf("listener url incorrect, got: %s, expected: https://listener.logz.io:8071", config.ListenerURL)
+	config.APIToken = testAccountToken
+	err = config.Validate()
+	if err != nil {
+		tester.Error("validation failed, one of api token or account token can be empty")
 	}
 
-	config.ListenerURL = testURL
-	config.Validate()
-	if config.ListenerURL != testURL {
-		tester.Error("listener url changed, should have stayed the same")
+	config.AccountToken = testAccountToken
+	config.APIToken = ""
+	err = config.Validate()
+	if err != nil {
+		tester.Error("validation failed, one of api token or account token can be empty")
+	}
+}
+
+func TestRegion(tester *testing.T) {
+	config := LogzioConfig{
+		AccountToken: testAccountToken,
+		APIToken:     testAccountToken,
+		Region:       "",
+	}
+
+	if config.ListenerURL() != "https://listener.logz.io:8071" {
+		tester.Errorf("listener url incorrect, got: %s, expected: https://listener.logz.io:8071", config.ListenerURL())
+	}
+
+	if config.APIURL() != "https://api.logz.io/v1/elasticsearch/_msearch" {
+		tester.Errorf("api url incorrect, got: %s, expected: https://api.logz.io/v1/elasticsearch/_msearch", config.APIURL())
+	}
+
+	config.Region = "na"
+	if config.ListenerURL() != "https://listener.logz.io:8071" {
+		tester.Errorf("listener url incorrect, got: %s, expected: https://listener.logz.io:8071", config.ListenerURL())
+	}
+	if config.APIURL() != "https://api.logz.io/v1/elasticsearch/_msearch" {
+		tester.Errorf("api url incorrect, got: %s, expected: https://api.logz.io/v1/elasticsearch/_msearch", config.APIURL())
+	}
+
+	config.Region = "eu"
+	if config.ListenerURL() != "https://listener-eu.logz.io:8071" {
+		tester.Errorf("listener url incorrect, got: %s, expected: https://listener.logz.io:8071", config.ListenerURL())
+	}
+	if config.APIURL() != "https://api-eu.logz.io/v1/elasticsearch/_msearch" {
+		tester.Errorf("api url incorrect, got: %s, expected: https://api-eu.logz.io/v1/elasticsearch/_msearch", config.APIURL())
 	}
 }
 
@@ -36,10 +73,13 @@ func TestParseConfig(tester *testing.T) {
 		tester.Errorf("error parsing config file: %s", err.Error())
 		return
 	}
-	if config.ListenerURL != testURL {
-		tester.Errorf("wrong listener, expected: testURL, got: %s", config.ListenerURL)
+	if config.Region != testRegion {
+		tester.Errorf("wrong listener, expected: testURL, got: %s", config.Region)
 	}
-	if config.AccountToken != testToken {
-		tester.Errorf("wrong account token, expected: testToken, got: %s", config.AccountToken)
+	if config.AccountToken != testAccountToken {
+		tester.Errorf("wrong account token, expected: testAccountTo	ken, got: %s", config.AccountToken)
+	}
+	if config.APIToken != testAPIToken {
+		tester.Errorf("wrong api token, expected: testApiToken, got: %s", config.AccountToken)
 	}
 }
